21585de727374db06bdc7b9b3e353b1b27ba00c1
Joseph Autorino <staircasesoul@gmail.com>
2021-04-02 00:21:36 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -38,7 +38,7 @@ describe('index', () => {
   })
 
 
-  it('calls console.warn("Danger!")', done => {
+  it('calls console.warn()', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
